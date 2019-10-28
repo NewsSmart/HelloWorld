4a0278a39cabed6b5b9a8b08ad1d953ada9e0417
zhuxinwen <zhuxinwen@macbook-air>
2019-10-28 15:14:29 +0800

    init project

---
+++ src/main/java/com/cmbc/helloworld/domain/App.java
@@ -0,0 +1,13 @@
+package com.cmbc.helloworld.domain;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
